4a36b27d0e51f2f3d411c6a1fcfa8bd78eb3a0f3
IanOC1994 <a9c6ezb0@students.codeinstitute.net>
2025-03-17 18:34:56 +0000

    Add random placement for ships

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Import the Flask class from the flask module
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
@@ -10,9 +11,18 @@ def create_grid():
     return [[' ' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
 
 
+def create_grid():
+    ships = 0
+    while ships < GRID_SIZE // 2:
+        x, y = random.randint(0, GRID_SIZE - 1), random.randint(0, GRID_SIZE - 1)
+        if grid[x][y] == '~':
+            grid[x][y] = 'S'
+            ships += 1
+    return grid
+
 @app.route('/')
 def index():
-    grid = create_grid()
+    grid = place_ships()
     return render_template('index.html', grid=grid)
 
 
